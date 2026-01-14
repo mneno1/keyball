37829951c7029ebf935a4ea68dad0d6fbaf37ccc
mneno1 <mnenoi.mobile@gmail.com>
2026-01-14 12:44:32 +0900

    Update config.h
    Fix redefined error.

--- qmk_firmware/keyboards/keyball/keyball44/keymaps/via/config.h
+++ qmk_firmware/keyboards/keyball/keyball44/keymaps/via/config.h
@@ -36,9 +36,8 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define TAP_CODE_DELAY 5
 
 #define POINTING_DEVICE_AUTO_MOUSE_ENABLE
-#define AUTO_MOUSE_DEFAULT_LAYER 1
+#define AUTO_MOUSE_DEFAULT_LAYER 5
 #define DYNAMIC_KEYMAP_LAYER_COUNT 6
 #define KEYBALL_CPI_DEFAULT 700
 #define KEYBALL_SCROLL_DIV_DEFAULT 5
-#define AUTO_MOUSE_DEFAULT_LAYER 5
 
